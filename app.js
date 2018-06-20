d4f8a5706274246da784f05ea03ac3983936c0bf
Stefan Schenk <swj.schenk@portofrotterdam.com>
2018-06-20 23:07:00 +0200

    Added CORS header

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var db = require('./db');
 
 var UserController = require('./user/UserController');
 var QuoteController = require('./quote/QuoteController');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/users', UserController);
 app.use('/quotes', QuoteController);
 
